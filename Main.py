059dc078c1a5b79ec0e01e1bf06b276a50e34452
Jakson Wainwright <jwainwright@GPAdmins-MacBook-Pro.local>
2020-05-28 11:24:47 -0600

    Created SCSS/CSS directories. Tested pushing/pulling/creating css files. Added some SCSS. corrected render-template call in flask

--- Main.py
+++ Main.py
@@ -6,39 +6,14 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
-@app.before_request
-def validate_session():
-    if 'username' in session:
-        return
-    else:
-        return render_template('login.html')
-
-
 @app.route('/')
 def render_index():
-    if 'username' in session:
-        return 'Logged in as %s' % escape(session['username'])
-    return 'You are not logged in'
-
-
-@app.route('/render_login', methods=['GET', 'POST'])
-def render_login():
-    if request.method == 'POST':
-        session['username'] = request.form['username']
-        return redirect(url_for('render_index'))
-    return render_template('login.html')
-
-
-@app.route('/logout')
-def logout():
-    # remove the username from the session if it's there
-    session.pop('username', None)
-    return redirect(url_for('render_index'))
+    return render_template('index.html')
 
 
 @app.route('/statistics')
 def render_statistics_page():
-    render_template('statistics.html')
+    return render_template('statistics.html')
 
 
 app.run()
